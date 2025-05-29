ed35ef84b0112a61d440990db8ba33fe9a210473
Harsh Narayan Jha <harshnj@proton.me>
2025-05-29 18:08:01 +0530

    add workflows

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // static build for now
+  output: "export",
+  basePath: "/freyza-web",
+  assetPrefix: "/freyza-web",
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
